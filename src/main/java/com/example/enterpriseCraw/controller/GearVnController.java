4719f3a957f8d743fdce77d0936d65617b03cd14
quynguyn <s3878636@rmit.edu.vn>
2023-05-21 13:08:37 +0700

    dynamic template

--- src/main/java/com/example/enterpriseCraw/controller/GearVnController.java
+++ src/main/java/com/example/enterpriseCraw/controller/GearVnController.java
@@ -4,10 +4,8 @@ import com.example.enterpriseCraw.model.GearVn;
 import com.example.enterpriseCraw.repo.GearVnRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,8 +21,20 @@ public class GearVnController {
         return gearVnRepo.findAll();
     }
 
-    @GetMapping("/{component}")
-    public List<GearVn> getProductsByComponent(@PathVariable String component) {
-        return gearVnRepo.findByComponent(component);
+    @GetMapping("/category/{component}")
+    public String getProductsByComponent(@PathVariable String component, Model model) {
+       List<GearVn> componentList = gearVnRepo.findByComponent(component);
+       model.addAttribute("product",componentList);
+       return "category";
     }
+
+
+//    @GetMapping("/{id}")
+//    public List<GearVn> getProductsByComponent(@ String component) {
+//        List <GearVn> componentList = gearVnRepo.findByComponent(component);
+//        model.addAttribute("products", componentList);
+//        return "product-list";
+//    }
+
+
 }
