47c42f637126d587292936465988b6950d2eb229
quynguyn <s3878636@rmit.edu.vn>
2023-05-21 11:24:08 +0700

    set up spring

---
+++ src/test/java/com/example/enterpriseCraw/EnterpriseCrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseCraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseCrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
